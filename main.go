32030f93642f9aa95d504903a7befc2870a165b1
Walter Gysin <walter@botmaker.com>
2022-01-25 07:40:26 -0300

    initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	http.ListenAndServe(":9090", nil)
+}
